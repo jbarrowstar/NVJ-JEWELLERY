9311dca4d253226ebf4a913980aacf74dc405448
PRAVIN2506 <pravin250604@gmail.com>
2025-11-12 22:08:16 +0530

    updated

--- electron/main.js
+++ electron/main.js
@@ -7,26 +7,35 @@ function createWindow() {
     height: 768,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: false, // Required for window.print() and react-to-print
+      nodeIntegration: false,  // Keep false unless you need Node in renderer
     },
   });
 
   win.loadFile(path.join(__dirname, '../frontend/dist/index.html'));
 
+  // Relaxed CSP for print preview compatibility
   win.webContents.session.webRequest.onHeadersReceived((details, callback) => {
-  callback({
-    responseHeaders: {
-      ...details.responseHeaders,
-      'Content-Security-Policy': [
-        "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self' http://localhost:3001"
-      ]
-    }
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Content-Security-Policy': [
+          "default-src 'self'; " +
+          "script-src 'self' 'unsafe-eval'; " + // Allow dynamic script execution
+          "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+          "style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+          "font-src 'self' https://fonts.gstatic.com; " +
+          "connect-src *; " + // Broader API access for dev/testing
+          "img-src * data: blob:;" // Allow blob URLs for print iframe
+        ]
+      }
+    });
   });
-});
-
 }
 
 app.whenReady().then(() => {
   createWindow();
+
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
